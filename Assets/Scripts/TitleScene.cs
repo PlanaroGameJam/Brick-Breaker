ab3b85d4d45a8264dd043f9970fc41fa6b0a4a3e
Seeker <yoshimitsu12@outlook.com>
2022-11-27 06:20:37 +0900

    タイトルの更新を行った

--- Assets/Scripts/TitleScene.cs
+++ Assets/Scripts/TitleScene.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScene : MonoBehaviour
 {
@@ -11,11 +13,18 @@ public class TitleScene : MonoBehaviour
     private PlayerController m_playerController;
     [SerializeField]
     private GameObject m_titleUI;
+    private Button m_startButton;
+    private Button m_howToPlayButton;
     // Start is called before the first frame update
     void Awake()
     {
         m_playerController = Instantiate(m_playerControllerObject, null).GetComponent<PlayerController>();
         m_titleUI = Instantiate(m_titleUI, GameObject.FindWithTag("Canvas").transform);
+        m_startButton = m_titleUI.GetComponentsInChildren<Button>().First();
+        m_howToPlayButton = m_titleUI.GetComponentsInChildren<Button>().Last();
+
+        m_startButton.onClick.AddListener(Start_Click);
+        m_howToPlayButton.onClick.AddListener(HowToPlay_Click);
     }
 
     // Update is called once per frame
@@ -29,4 +38,17 @@ public class TitleScene : MonoBehaviour
         m_playerController.Disable();
         SceneManager.LoadSceneAsync("Tutorial");
     }
+
+    private void Start_Click()
+    {
+        m_playerController.Disable();
+        SceneManager.LoadSceneAsync("Play");
+    }
+
+    private void HowToPlay_Click()
+    {
+        m_playerController.Disable();
+        SceneManager.LoadSceneAsync("Tutorial");
+    }
+
 }
