199b30b69d11faf578b3a088149af871c13c3815
Seeker <yoshimitsu12@outlook.com>
2022-11-26 19:59:38 +0900

    シンプルなオブジェクトを作った
    作成した物
    ・パドル
    ・ボール
    ・ブロック

---
+++ Assets/PlayScene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayScene : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_playerControllerObject;
+    private PlayerController m_playerController;
+
+    void Awake()
+    {
+        m_playerController = Instantiate(m_playerControllerObject, null).GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
